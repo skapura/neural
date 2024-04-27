c7d3572254e8f227a999e2da7da571ffc1c0090e
Nick Skapura <84984964+skapura@users.noreply.github.com>
2024-04-27 18:11:05 -0400

    Find entropy based cut points.

--- main2.py
+++ main2.py
@@ -1,7 +1,10 @@
+import math
+
 from tensorflow.keras import datasets, layers, models, backend
 import tensorflow as tf
 import ssl
 import numpy as np
+import random
 from mlutil import makeDebugModel
 
 
@@ -31,6 +34,44 @@ def layerNames(model):
     return layernames
 
 
+def entropy(ds):
+    if len(ds) == 0:
+        return 0.0
+    correctpct = len([v for v in ds if v[1]]) / len(ds)
+    incorrectpct = len([v for v in ds if not v[1]]) / len(ds)
+    if correctpct == 0.0 or incorrectpct == 0.0:
+        ent = 0.0
+    else:
+        ent = -1 * (correctpct * math.log2(correctpct) + incorrectpct * math.log2(incorrectpct))
+    return ent
+
+
+def cutPoints(fmaps, iscorrect):
+    cutpoints = []
+    vals = list(zip(fmaps, iscorrect))
+    tvals = np.unique(fmaps)
+    testpoints = random.sample(range(len(tvals)), min(100, len(tvals)))
+    mincut = -1
+    mininfo = 99999
+    d1size = 0
+    d2size = 0
+    for tp in testpoints:
+        cutpoint = tvals[tp]
+        d1 = [v for v in vals if v[0] < cutpoint]
+        d2 = [v for v in vals if v[0] >= cutpoint]
+        e1 = entropy(d1)
+        e2 = entropy(d2)
+        info = (len(d1) / len(vals)) * e1 + (len(d2) / len(vals)) * e2
+        if info <= mininfo:
+            mincut = cutpoint
+            mininfo = info
+            d1size = len(d1)
+            d2size = len(d2)
+    cutpoints.append(mincut)
+    print('best info:' + str(mininfo) + ', cutpoint:' + str(mincut) + ', d1:' + str(d1size) + ', d2:' + str(d2size))
+    return cutpoints
+
+
 ssl._create_default_https_context = ssl._create_unverified_context
 
 (orig_train_images, train_labels), (orig_test_images, test_labels) = datasets.cifar10.load_data()
@@ -47,8 +88,14 @@ print(model.summary())
 debugmodel = makeDebugModel(model)
 layernames = layerNames(debugmodel)
 outs = debugmodel.predict(test_images)
+ymaxpreds = [np.argmax(x) for x in outs[-1]]
+labels = [l[0] for l in test_labels]
+iscorrect = [ymaxpreds[i] == labels[i] for i in range(len(labels))]
+labelinfo = list(zip(ymaxpreds, labels, iscorrect))
 convouts = [o for o in outs if len(o.shape) == 4]   # get only CNN layers
 colnames, activations = featureActivations(convouts, layernames)
+
+cuts = [cutPoints(a, iscorrect) for a in activations]
 
 model = models.Sequential()
 model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
