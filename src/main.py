dac7ed4b8fdc238f85a4c05c5eb777be402096fd
Nick Skapura <84984964+skapura@users.noreply.github.com>
2024-09-26 19:31:53 -0400

    Fix azure vm.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
 from experiment_layer import run
-
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import tensorflow as tf
+import logging
+logger = tf.get_logger()
+logger.setLevel(logging.ERROR)
 
 
 run()
