e660b1d07f60f47a3c3b88bcb98afe80ca864dec
Nick Skapura <84984964+skapura@users.noreply.github.com>
2024-11-01 09:48:24 -0400

    Mody sync script to copy list of files.

--- scripts/syncvm.sh
+++ scripts/syncvm.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-if [[ $1 = "-a" ]]; then
+if [[ $1 = "-a" ]]; then  # copy all files in src/ dir
   scp -i ~/.ssh/azure -r src/*.py nskapura@172.210.252.176:/home/nskapura/neural/src
-elif [[ -n $1 ]]; then
-  scp -i ~/.ssh/azure $1 nskapura@172.210.252.176:/home/nskapura/neural/$2
-else
+elif [[ $# -gt 0 ]]; then # copy list of files
+  scp -i ~/.ssh/azure "$@" nskapura@172.210.252.176:/home/nskapura/neural/src
+else  # copy only changes in git repo
   scp -i ~/.ssh/azure `git status --porcelain | awk 'match($1, "M"){print $2}'` nskapura@172.210.252.176:/home/nskapura/neural/src
-  echo updates
 fi
